e34cef412f1e4bbd0573dd5660f18ed187d21c65
irnafitriani <irna.fitriani@gmail.com>
2016-12-06 15:24:29 +0700

    Create Login Screen UI

---
+++ RMS_v0.1/EmployeeListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EmployeeListViewController.swift
+//  RMS_v0.1
+//
+//  Created by mitrais on 12/6/16.
+//  Copyright © 2016 MTMAC19. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeListViewController: UIViewController {
+
+}
