4c9cacf31fe1fa684c65d10b263cfca98aeeffa3
irnafitriani <irna.fitriani@gmail.com>
2016-12-08 14:43:56 +0700

    - create group for files - create employee detail fix

---
+++ RMS_v0.1/Controller/EmployeeDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EmployeeDetailViewController.swift
+//  RMS_v0.1
+//
+//  Created by mitrais on 12/8/16.
+//  Copyright © 2016 MTMAC19. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeDetailViewController: UIViewController {
+
+}
